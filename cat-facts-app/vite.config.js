dc651f1ba8811ece2d65dd25d2d91c9d4730c3af
KICube <bogdanlopatkin9@gmail.com>
2026-09-09 22:16:54 +0300

    fixed: dumbass network error

--- cat-facts-app/vite.config.js
+++ cat-facts-app/vite.config.js
@@ -1,7 +1,15 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://catfact.ninja',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
